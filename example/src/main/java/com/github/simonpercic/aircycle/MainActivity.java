fd03923894b9e4fb3ace6ac1d3233b6e24f618d7
Simon Perčič <simon@percic.si>
2016-08-23 19:39:30 +0200

    Basic example app

---
+++ example/src/main/java/com/github/simonpercic/aircycle/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.simonpercic.aircycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author Simon Percic <a href="https://github.com/simonpercic">https://github.com/simonpercic</a>
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

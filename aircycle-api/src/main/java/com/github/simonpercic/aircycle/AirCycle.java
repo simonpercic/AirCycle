323ec5cf962e59eaed0375aeb7243b43eb285952
Simon Perčič <simon@percic.si>
2016-09-10 20:46:35 +0200

    Javadoc lint

--- aircycle-api/src/main/java/com/github/simonpercic/aircycle/AirCycle.java
+++ aircycle-api/src/main/java/com/github/simonpercic/aircycle/AirCycle.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 /**
  * Bind an Activity lifecycle callback method to a listener.
- * <p/>
+ * <p>
  * Supported lifecycle callbacks are:
  * - onCreate() (with optional Bundle savedInstanceState argument)
  * - onStart()
@@ -16,12 +16,12 @@ import java.lang.annotation.Target;
  * - onStop()
  * - onSaveInstanceState() (with optional Bundle outState argument)
  * - onDestroy()
- * <p/>
+ * <p>
  * The annotation processor will generate a binding class for the Activity, named `YourActivity`AirCycle with a
  * static `bind` method.
  * You MUST call `YourActivityAirCycle.bind(this)` BEFORE calling `super.onCreate(savedInstanceState)`,
  * otherwise the bound listener will NOT receive the first onCreate call for the Activity.
- * <p/>
+ * <p>
  * Valid lifecycle method names are:
  * onCreate() / onActivityCreated(),
  * onStart() / onActivityStarted(),
@@ -30,7 +30,7 @@ import java.lang.annotation.Target;
  * onStop() / onActivityStopped(),
  * onSaveInstanceState() / onActivitySaveInstanceState(),
  * onDestroy() / onActivityDestroyed()
- * <p/>
+ * <p>
  * See https://github.com/simonpercic/AirCycle for more info.
  *
  * @author Simon Percic <a href="https://github.com/simonpercic">https://github.com/simonpercic</a>

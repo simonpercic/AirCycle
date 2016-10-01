3c1bd5ad6fb824abe96b5bceddacaab5a13ab507
Simon Perčič <simon@percic.si>
2016-10-01 20:30:37 +0200

    Ignore annotation example in CustomListener

--- example/src/main/java/com/github/simonpercic/example/aircycle/activity/a7/CustomListener.java
+++ example/src/main/java/com/github/simonpercic/example/aircycle/activity/a7/CustomListener.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.github.simonpercic.aircycle.Ignore;
+
 import timber.log.Timber;
 
 /**
@@ -23,6 +25,19 @@ public class CustomListener {
 
     public void onCreate(Bundle bundle) {
         Timber.d("onCreate bundle: %s", bundle);
+    }
+
+    @Ignore
+    public void onStart() {
+        Timber.d("onStart");
+    }
+
+    @Ignore public void onResume(CustomListenerActivity activity) {
+        Timber.d("onResume activity: %s", activity);
+    }
+
+    public void onResume() {
+        Timber.d("onResume");
     }
 
     public void onSaveInstanceState(AppCompatActivity activity, Bundle bundle) {
